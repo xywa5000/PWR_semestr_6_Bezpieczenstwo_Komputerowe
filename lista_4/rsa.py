5fde166955036cbd18ac5d93e8e3da1b51907db7
xywa5000 <xywa5000@gmail.com>
2024-05-26 11:46:40 +0200

    List 4
    Prime numbers detector improv

--- lista_4/rsa.py
+++ lista_4/rsa.py
@@ -1,6 +1,50 @@
 import random
 
-# TODO: improve this func
+
+def miller_rabin_test(n, k=20):
+    """
+    Przeprowadza test Millera-Rabina na liczbie n z k iteracjami.
+    Zwraca True, jeśli n jest prawdopodobnie pierwsza, w przeciwnym razie False.
+    
+    n: liczba do sprawdzenia
+    k: liczba iteracji testu
+    """
+    # Jeśli n jest mniejsze niż 2 lub jest parzysta (z wyjątkiem 2), to nie jest pierwsza
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    
+    # Znajdź d i r takie, że n-1 = d * 2^r
+    r, d = 0, n - 1
+    while d % 2 == 0:
+        d //= 2
+        r += 1
+    
+    def is_composite(a):
+        """
+        Sprawdza, czy n jest złożona dla danego świadka a.
+        """
+        x = pow(a, d, n)
+        if x == 1 or x == n - 1:
+            return False
+        for _ in range(r - 1):
+            x = pow(x, 2, n)
+            if x == n - 1:
+                return False
+        return True
+    
+    # Przeprowadź k iteracji testu
+    for _ in range(k):
+        a = random.randint(2, n - 2)
+        if is_composite(a):
+            return False
+    
+    return True
+
+
 def is_prime(n):
     if n <= 1 or (n % 2 == 0 and n > 2): 
         return False
@@ -35,7 +79,7 @@ def multiplicative_inverse(e, phi):
         return d + phi
 
 def generate_keypair(p, q):
-    if not (is_prime(p) and is_prime(q)):
+    if not (miller_rabin_test(p) and miller_rabin_test(q)):
         raise ValueError('Both numbers must be prime.')
     elif p == q:
         raise ValueError('p and q cannot be equal')
@@ -49,6 +93,16 @@ def generate_keypair(p, q):
     d = multiplicative_inverse(e, phi)
     return ((n, e), (n, d))
 
+def modular_exponentiation(a, k, n):
+    result = 1
+    a = a % n
+    while k > 0:
+        if (k % 2) == 1:  # Jeśli k jest nieparzyste, pomnóż wynik przez a
+            result = (result * a) % n
+        k = k >> 1  # Podziel k przez 2
+        a = (a * a) % n  # Podnieś a do kwadratu
+    return result
+
 def attack(n, e, d):
     # 1
     kphi = d * e - 1
@@ -61,11 +115,13 @@ def attack(n, e, d):
     # 3
     a = 2
     p=1
-    while (a<1000):
+    while (a<100):
+        print(a)
         k = t
         flag = False
         while ( k<kphi):
-            x = (a ** k) % n
+            #x = (a ** k) % n
+            x = modular_exponentiation(a, k, n)
             if (x!=t and x!=n-1 and (x*x)%n==1):
                 p = gcd(x-1, n)
                 flag = True
@@ -80,8 +136,8 @@ def attack(n, e, d):
 
 if __name__ == '__main__':
 
-    p = 13
-    q = 17
+    p = 8521
+    q = 2129
 
     publicA, privateA = generate_keypair(p, q)
     publicB, privateB = generate_keypair(p, q)
